2a5c332ef5ee8a8bc5b65f901e78603bd5578f1f
normstraker <88446816+normstraker@users.noreply.github.com>
2022-09-14 16:37:08 -0500

    Get all the name elements from the DOM

--- app.js
+++ app.js
@@ -12,3 +12,15 @@ document
     let searchQuery = event.target.value.toLowerCase();
     console.log(searchQuery);
   });
+
+/*
+    Task: Get all the name elements from the DOM
+    1. Create a new 'let' called 'allNamesDOMCollection'
+    2. Store all the elements of classname 'name' in 'allNamesDOMCollection'
+    3. Log to the console 'allNamesDOMCollection' to demonstrate that an HTMLCollection exists
+    4. Log out the first index of allNamesDOMCollection and add 'textContent' to view a name
+*/
+
+let allNamesDOMCollection = document.getElementsByClassName("name");
+console.log(allNamesDOMCollection);
+console.log(allNamesDOMCollection[0].textContent);
