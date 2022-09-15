8be5721b7da7287dc41bfab482f53bf9b3d1bf6a
normstraker <88446816+normstraker@users.noreply.github.com>
2022-09-15 11:11:02 -0500

    Comparing the search query vs. the DOM names

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
+// Task: Comparing the search query vs. the DOM names
+//     1. Create a conditional that checks if currentName is equal to the user's search query!
+//     2. If our conditional returns true, set the display style on 'allNamesDOMCollection[counter]' to be a block element
+//     3. If our conditional falls into the else, we have no match! So set the display style to 'none'
+
+//     Helpers: use the methods 'includes' for objective 1
+//     - The includes() method determines whether one string may be found within another string, returning true or false as appropriate.
+
 document
   .getElementById("searchInput")
   .addEventListener("keyup", function (event) {
     let searchQuery = event.target.value.toLowerCase();
-    console.log(searchQuery);
+    let allNamesDOMCollection = document.getElementsByClassName("name");
+
+    for (let i = 0; i < allNamesDOMCollection.length; i++) {
+      const currentName = allNamesDOMCollection[i].textContent.toLowerCase();
+
+      if (currentName.includes(searchQuery)) {
+        allNamesDOMCollection[i].style.display = "block";
+      } else {
+        allNamesDOMCollection[i].style.display = "none";
+      }
+    }
   });
-
-let allNamesDOMCollection = document.getElementsByClassName("name");
-// console.log(allNamesDOMCollection[0].textContent);
-
-/*
-    Task: Loop over all our names
-    1. Create a new for loop with a incremental counter, called 'counter'
-    2. Make sure the counter is less than the allNamesDOMCollection.length in the loop conditional
-    3. Declare a const called 'currentName' and store the textContext
-    4. Make sure the 'currentName' is lowercase
-    5. Log out the 'currentName' so we can see all the names from the loop!
-*/
-
-for (let i = 0; i < allNamesDOMCollection.length; i++) {
-  const currentName = allNamesDOMCollection[i].textContent.toLowerCase();
-  console.log(currentName);
-}
